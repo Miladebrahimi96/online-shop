054d001931e0757f02e2d264f2fd5efe9fe1bcb0
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-16 18:15:00 +0430

    ProductDetails component created

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,17 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
+
+//APIs
+import { getProduct } from '../services/api';
 
 const ProductDetails = () => {
 
-    const [products, setProducts] = useState([])
+    const params = useParams();
+    const id = params.id;
+
+    const [product, setProduct] = useState([]);
 
     useEffect(() => {
-        
+        const fetchProduct = async () => {
+            setProduct(await getProduct(id));
+        }
+        fetchProduct();
     },[])
+
+    const {image, title, price, category, description} = product;
 
     return (
         <div>
-            
+            <img src={image} alt="product_image"/>
+            <div>
+                <h3>{title}</h3>
+                <p>{description}</p>
+                <p><span>Category:</span> {category}</p>
+                <div>
+                    <span>${price}</span>
+                    <Link to="/products">Back to Shop</Link>
+                </div>
+            </div>
         </div>
     );
 };
