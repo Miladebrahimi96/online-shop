27bf681bcf94febf738f48ab9f2ca95594083626
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-16 21:47:02 +0430

    ProductDetails styled

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 
+//styles
+import style from "./ProductDetails.module.css";
+
 //APIs
 import { getProduct } from '../services/api';
 
@@ -21,14 +24,14 @@ const ProductDetails = () => {
     const {image, title, price, category, description} = product;
 
     return (
-        <div>
-            <img src={image} alt="product_image"/>
-            <div>
+        <div className={style.container}>
+            <img className={style.image} src={image} alt="product_image"/>
+            <div className={style.textContainer}>
                 <h3>{title}</h3>
-                <p>{description}</p>
-                <p><span>Category:</span> {category}</p>
-                <div>
-                    <span>${price}</span>
+                <p className={style.description}>{description}</p>
+                <p className={style.category}><span>Category:</span> {category}</p>
+                <div className={style.buttonContainer}>
+                    <span className={style.price}>${price}</span>
                     <Link to="/products">Back to Shop</Link>
                 </div>
             </div>
