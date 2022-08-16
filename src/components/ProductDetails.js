c22d0346b5e2eb9527b9c3c3f06f73ab8366ff78
Milad Ebrahimi <milad.eb6868@gmail.com>
2022-08-16 18:03:00 +0430

    A function for tehcing single produtc base on id declared in api.js

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+const ProductDetails = () => {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        
+    },[])
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProductDetails;
--- src/services/api.js
+++ src/services/api.js
@@ -7,4 +7,9 @@ const getProducts = async () => {
     return response.data
 }
 
+const getProduct = async (id) => {
+    const response = await axios.get(`${BASE_URL}/products/${id}`);
+    return response.data;
+}
+
 export { getProducts };
